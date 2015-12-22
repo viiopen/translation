d068bd4be1066ac4cbb0a53bddc2b98bb554bba2
Ben Matics <bmatics@viimed.com>
2015-12-22 12:38:00 -0500

    removing tests, disabling csvdumper

--- Dumper/CsvFileDumper.php
+++ Dumper/CsvFileDumper.php
@@ -28,17 +28,20 @@ class CsvFileDumper extends FileDumper
      */
     public function format(MessageCatalogue $messages, $domain = 'messages')
     {
-        $handle = fopen('php://memory', 'rb+');
+	// BMM - not allowing due to security concerns. 12/22/2015
+	throw new \Exception('Dumping as CSV not supported');
 
-        foreach ($messages->all($domain) as $source => $target) {
-            fputcsv($handle, array($source, $target), $this->delimiter, $this->enclosure);
-        }
-
-        rewind($handle);
-        $output = stream_get_contents($handle);
-        fclose($handle);
-
-        return $output;
+//        $handle = fopen('php://memory', 'rb+');
+//
+//        foreach ($messages->all($domain) as $source => $target) {
+//            fputcsv($handle, array($source, $target), $this->delimiter, $this->enclosure);
+//        }
+//
+//        rewind($handle);
+//        $output = stream_get_contents($handle);
+//        fclose($handle);
+//
+//        return $output;
     }
 
     /**
